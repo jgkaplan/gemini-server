2daae4ced43c9778e21b941782c3daa9f4c63244
Joshua Kaplan <jgk83@cornell.edu>
2021-07-12 10:52:49 -0500

    Removed hash symbol for compatability with earlier versions of node

--- Response.js
+++ Response.js
@@ -6,66 +6,66 @@ const { STATUS } = require('./utils.js');
 mime.define({'text/gemini': ['gemini', 'gmi']})
 
 class Response {
-    #status = null;
-    #meta = "";
-    #body = null;
+    _status = null;
+    _meta = "";
+    _body = null;
 
-    #setMeta(m){
-      this.#meta = truncate(m, 1024);
+    _setMeta(m){
+      this._meta = truncate(m, 1024);
     }
     constructor(status = null, meta = null){
-      this.#status = status;
-      this.#setMeta(meta);
+      this._status = status;
+      this._setMeta(meta);
     }
 
     status(s){
-      this.#status = s;
+      this._status = s;
       return this;
     }
 
     getStatus(){
-      return this.#status;
+      return this._status;
     }
 
     data(d, mimeType='text/plain'){
       this.status(STATUS._20);
-      this.#body = d;
-      this.#setMeta(mimeType);
+      this._body = d;
+      this._setMeta(mimeType);
       return this;
     }
     //for success, The <META> line is a MIME media type which applies to the response body.
     //for redirect, <META> is a new URL for the requested resource. The URL may be absolute or relative.
     //for 4* and 5*, The contents of <META> may provide additional information on the failure, and should be displayed to human users.
     file(filename){ // might throw error if file doesn't exist
-      this.#body = fs.readFileSync(filename);
+      this._body = fs.readFileSync(filename);
       this.status(STATUS._20);
-      this.#setMeta(mime.getType(filename));
+      this._setMeta(mime.getType(filename));
       return this;
     }
 
     input(prompt, sensitive=false){ //client should re-request same url with input as a query param
       this.status(sensitive?STATUS._11:STATUS._10);
-      this.#setMeta(prompt);
+      this._setMeta(prompt);
       return this;
     }
 
     certify(info="Please include a certificate."){ //request certificate from client
-      this.#setMeta(info);
+      this._setMeta(info);
       this.status(STATUS._60);
       return this;
     }
 
     redirect(url){
       this.status(STATUS._30);
-      this.#setMeta(url);
+      this._setMeta(url);
     }
 
     format_header(){
-      return `${this.#status} ${this.#meta}\r\n`;
+      return `${this._status} ${this._meta}\r\n`;
     }
 
     format_body(){
-      return `${this.#body}\r\n`;
+      return `${this._body}\r\n`;
     }
 }
 
